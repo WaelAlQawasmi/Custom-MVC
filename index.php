4d001fc49127170c43fe577642b8dc53128e6320
waelalq <wael.abuomr@gmail.com>
2023-12-16 15:14:36 +0200

    routing and documantation

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 
 <?php
+/**
+ * @copyright 2023 Wael Al Qawasmi
+ * @author Wael Al Qawasmi <wael.alqwasmi@yahoo.com>
+ */
 // to get path without query string
 $path= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$pathArray= explode('/',$path);
 
-// the path should be in this format => controller/action
-$controller=$pathArray[2];
-$action=$pathArray[3];
+// Get the directory where the PHP script is located
+$scriptDirectory = dirname($_SERVER['SCRIPT_NAME']);
+// Remove the script directory from the full URL
+$path = str_replace($scriptDirectory, '', $path);
 
 
 
-require "Controllers/$controller.php";
+require 'src/Router.php';
+$router= new Router();
+$router->add('/prodacts',['controller' => 'prodacts', 'action' =>'index']);
+$router->add('/home/contactus',['controller' => 'home', 'action' =>'contactus']);
+$router->add('/',['controller' => 'home', 'action' =>'index']);
+
+$pathParameters=$router->match($path);
+if (!$pathParameters)
+   exit( '404 The Page Not Found');
+
+$controller=$pathParameters['controller'];
+$action=$pathParameters['action'];
+require "src/Controllers/$controller.php";
 $controllerObject = new $controller();
 
 $controllerObject->$action();
---
+++ src/Controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Wael Al Qawasmi <wael.alqwasmi@yahoo.com> 
+ */
+class Home
+{
+   public function index (){
+      echo 'index page';
+   }
+
+   public function contactUs() {
+      echo 'content us at oyr website';
+   }
+}
