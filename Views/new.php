e25b866fa6d795daa67c864ace4e6fc5c3545b2f
waelalq <wael.abuomr@gmail.com>
2024-01-08 21:24:59 +0200

    forms

---
+++ Views/new.php
@@ -0,0 +1,8 @@
+<h1> Creat new  prdact </h1>
+    <form action="" method="post">
+        <label> prodact name </label>
+        <input type="text" name="name" id="">
+        <button type="submit">submit</button>
+    </form>
+</body>
+</html>
