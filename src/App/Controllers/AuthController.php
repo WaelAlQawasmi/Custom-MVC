890e291f5f2afd9b6813bb3bd1bb880886d0789b
waelalq <wael.abuomr@gmail.com>
2024-01-24 20:05:26 +0200

    login

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -4,17 +4,36 @@
  */
 namespace App\Controllers;
 
+use App\Models\User;
 use Framework\controller;
 use Framework\Response;
 
 class AuthController extends controller
 {
- 
+   
+   private $user;
+   public function  __construct(  User $user ) {
+     $this->user=$user;
+   }
 
    public function login() {
 
-    $this->request->post['email'];
-    $this->request->post['password'];
+    $email=$this->request->post['email'];
+    $password=$this->request->post['password'];
+    $user=$this->user->selectFirst(['*'],['password'=>hash("sha256", $password), 'email'=>$email],['=','='],['and']);
+    if(empty($user)){
+      $this->response->setStatusCode(401);
+      return $this->redirect("./home");
+    }
+    $this->response->setStatusCode(200);
+
+    return $this->redirect("./prodacts");
+
+
+
+
+
+    
       
    }
     public function register() {
